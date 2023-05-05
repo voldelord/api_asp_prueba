f3aba40e57741a44b70c8ecd6d47172e56df3f41
luis <voldelord26@gmail.com>
2023-05-05 14:12:14 -0400

    actualizacion de los controladores y de las dependencias

--- apiaprendizaje_api/Data/AplicationDBContext.cs
+++ apiaprendizaje_api/Data/AplicationDBContext.cs
@@ -1,6 +1,11 @@
-﻿namespace apiaprendizaje_api.Data
+﻿
+using apiaprendizaje_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiaprendizaje_api.Data
 {
-    public class AplicationDBContext
+    public class AplicationDBContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
     }
 }
